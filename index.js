bf3968543d793a27be8eacf320bac8e8903513b6
Rony <frrony1996@gmail.com>
2022-04-27 15:26:07 +0500

    request for single volunteer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors())
@@ -27,6 +28,13 @@ async function run() {
             const query = {}
             const cursor = volunteerCollection.find(query)
             const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.get('/volunteer/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const result = await volunteerCollection.findOne(query)
             res.send(result)
         })
         app.post('/registered', async (req, res) => {
